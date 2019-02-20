28f74f1b4f67a0d46e6d48f5196ee37c7568027a
degroenli <degroen@wgc.school.nz>
2019-02-20 14:32:32 +1300

    beginning calculator

--- calculator.py
+++ calculator.py
@@ -3,4 +3,22 @@ from tkinter import*
 root = Tk()
 root.title("Lily's Calculator")
 
+def add_to_input_text(next_val):
+    current_text = input_text.get()
+    input_text.set(current_text+next_val)
+
+def answer():
+    ans = eval(input_text.get())
+    answer_value.set(ans)
+
+def clear():
+    input_text.set("")
+    answer_value.set("")
+
+input_text = StringVar()
+input_text.set("")
+
+answer_value = StringVar()
+answer_value.set("")
+
 root.mainloop()
